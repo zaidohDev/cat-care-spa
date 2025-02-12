bb4057b4a65af71d4b978d8b065e03162ba35988
ZaidoDev <danfealmeida@gmail.com>
2025-02-12 12:35:42 -0300

    final commit

--- public/script.js
+++ public/script.js
@@ -5,4 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
   menuToggle.addEventListener("click", function () {
     menu.classList.toggle("show");
   });
+
+  // Selecionando o elemento de áudio
+  const audioElement = document.getElementById("audio");
+
+  // Tentar reproduzir automaticamente
+  try {
+    audioElement.play();
+  } catch (error) {
+    console.log("Autoplay bloqueado, aguardando interação do usuário...");
+  }
+
+  // Caso o autoplay seja bloqueado, começamos a reprodução após interação com o corpo da página
+  document.body.addEventListener("click", function () {
+    audioElement.play().catch((error) => {
+      console.error("Erro ao tentar reproduzir o áudio:", error);
+    });
+  });
 });
